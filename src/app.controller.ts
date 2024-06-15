cc6e6e77047c990d9c4eba581e0e432d55cc3ad7
Kyure_A <49436968+Kyure-A@users.noreply.github.com>
2024-06-15 20:08:37 +0900

    aaa

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+// import * as line from "@line/bot-sdk";
+import { WebhookEvent, messagingApi, ClientConfig } from "@line/bot-sdk";
+import { Controller, Get, Post, Body } from '@nestjs/common';
 import { AppService } from './app.service';
+import { CHANNEL_ACCESS_TOKEN, CHANNEL_SECRET } from "secret";
+
+const config: ClientConfig = {
+  channelAccessToken: CHANNEL_ACCESS_TOKEN,
+  channelSecret: CHANNEL_SECRET
+}
 
 @Controller()
 export class AppController {
@@ -8,5 +16,26 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('webhook')
+  async handleWebhook(@Body('events') events: WebhookEvent[]): Promise<void> {
+    const client = new messagingApi.MessagingApiClient(config);
+    console.log(events);
+    
+    events.map(async (event) => {
+      
+      if (!(event.type === "message" && event.message.type === "text")) return;
+      
+      await client.replyMessage({
+        replyToken: event.replyToken,
+        messages: [
+          {
+            type: 'text',
+            text: event.message.text
+          }
+        ]
+      })
+    })
   }
 }
