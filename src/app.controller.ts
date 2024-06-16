ebb441f2eacac2ac1a4741c1e87b4379e25efcac
Kyure_A <49436968+Kyure-A@users.noreply.github.com>
2024-06-17 01:15:57 +0900

    style: format by prettier

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,21 +1,23 @@
-import { WebhookEvent, messagingApi, ClientConfig } from "@line/bot-sdk";
+import { WebhookEvent, messagingApi, ClientConfig } from '@line/bot-sdk';
 import { Controller, Get, Post, Body } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-  
+
   @Post('webhook')
   // FIXME: events を Schema で validation すべき
   async handleWebhook(@Body('events') events: WebhookEvent[]): Promise<void> {
     console.log(events);
-    
-    events.map(async (event) => { 
-      if (!(event.type === "message" && event.message.type === "text")) return;
 
-      const message = await this.appService.getAiInteraction(event.message.text);
-      
+    events.map(async (event) => {
+      if (!(event.type === 'message' && event.message.type === 'text')) return;
+
+      const message = await this.appService.getAiInteraction(
+        event.message.text,
+      );
+
       this.appService.sendMessage(event.replyToken, message);
     });
   }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,35 +1,38 @@
 import { Injectable } from '@nestjs/common';
-import OpenAI from "openai";
-import { role } from "static/role"
-import { CHANNEL_ACCESS_TOKEN, CHANNEL_SECRET, OPENAI_TOKEN } from "secret";
-import { messagingApi, ClientConfig } from "@line/bot-sdk";
+import OpenAI from 'openai';
+import { role } from 'static/role';
+import { CHANNEL_ACCESS_TOKEN, CHANNEL_SECRET, OPENAI_TOKEN } from 'secret';
+import { messagingApi, ClientConfig } from '@line/bot-sdk';
 
 const config: ClientConfig = {
   channelAccessToken: CHANNEL_ACCESS_TOKEN,
-  channelSecret: CHANNEL_SECRET
-}
+  channelSecret: CHANNEL_SECRET,
+};
 
 @Injectable()
 export class AppService {
   async sendMessage(replyToken: string, message: string): Promise<void> {
     const client = new messagingApi.MessagingApiClient(config);
-    
+
     await client.replyMessage({
       replyToken: replyToken,
       messages: [
         {
           type: 'text',
-          text: message
-        }
-      ]
+          text: message,
+        },
+      ],
     });
   }
-  
+
   async getAiInteraction(message: string): Promise<string> {
-    const openai = new OpenAI({apiKey: OPENAI_TOKEN});
+    const openai = new OpenAI({ apiKey: OPENAI_TOKEN });
     const completion = await openai.chat.completions.create({
-      messages: [{ role: "system", content: role }, {role: "user", content: message}],
-      model: "gpt-4o",
+      messages: [
+        { role: 'system', content: role },
+        { role: 'user', content: message },
+      ],
+      model: 'gpt-4o',
     });
 
     return completion.choices[0].message.content;
